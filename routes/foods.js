1df6862175d2aa2f4b989b918b33f214cb9b0077
reddy024 <156496348+reddy024@users.noreply.github.com>
2024-03-27 16:26:42 -0500

    server down

--- routes/foods.js
+++ routes/foods.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const router = express.Router();
- 
+
 const foods = [
-  {name:'biryani',category:'meals',price:'500'},
-  {name:'icecream',category:'refreshing items',price:'400'},
-  {name:'halwa',category:'sweets',price:'1000'},
+  { name: 'biryani', category: 'meals', price: '500' },
+  { name: 'icecream', category: 'refreshing items', price: '400' },
+  { name: 'halwa', category: 'sweets', price: '1000' },
 ];
- 
+
 router.get('/', (req, res) => {
-  res.render('foods', { title: 'Search Results for', foods });
+  res.render('foods', { title: 'Searching for Foods', foods });
 });
- 
+
 module.exports = router;
