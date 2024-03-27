15d8231c2ad8fc4fcdc471372ff1f26194455455
reddy024 <156496348+reddy024@users.noreply.github.com>
2024-03-27 15:43:04 -0500

    foods

--- routes/foods.js
+++ routes/foods.js
@@ -1,18 +1,14 @@
-var express = require('express');
-var router = express.Router();
-
-
+const express = require('express');
+const router = express.Router();
+ 
 const foods = [
   {name:'biryani',category:'meals',price:'500'},
   {name:'icecream',category:'refreshing items',price:'400'},
   {name:'halwa',category:'sweets',price:'1000'},
 ];
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('foods', { title: 'Serch results', foods });
+ 
+router.get('/', (req, res) => {
+  res.render('foods', { title: 'Search Results', foods });
 });
-
+ 
 module.exports = router;
-
-
