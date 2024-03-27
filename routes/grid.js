52c9b0e12c697e8a6a0343829fe1cabbce050387
reddy024 <156496348+reddy024@users.noreply.github.com>
2024-03-27 18:08:58 -0500

    title

--- routes/grid.js
+++ routes/grid.js
@@ -1,12 +1,7 @@
-const express = require('express');
-const router = express.Router();
- 
-/* GET grid page. */
 router.get('/', function(req, res, next) {
-  const query = req.query;
-  console.log(`rows ${query.rows}`);
-  console.log(`cols ${query.cols}`);
-  res.render('grid', { title2: 'Grid', query: req.query });
-});
- 
-module.exports = router;
+    const query = req.query;
+    console.log(`rows ${query.rows}`);
+    console.log(`cols ${query.cols}`);
+    res.render('grid', { title: 'Grid', query: req.query }); // Change to title
+  });
+  
